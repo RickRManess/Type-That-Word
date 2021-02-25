6bf2f24cab326aabb4333633c35c23bc9b4dd3ec
Richard Maness <rickrmaness@gmail.com>
2021-02-25 16:08:39 -0500

    Added comments in for all lines of code

--- Type That Word/Program.cs
+++ Type That Word/Program.cs
@@ -9,22 +9,29 @@ namespace Type_That_Word
         {
 
 
-            // Initialize the Words List
+           //Variable for Random index
             int myRandomIndex = 0;
-            var wordList = new List<string>(new[] { "apple", "bird", "cat", "dog", "elephant", "fish", "goat"});
+            //Initialize the wordlist with the words to choose from
+            var wordList = new List<string>(new[] { "apple", "bird", "cat", "dog", "elephant", "fish", "goat" });
+            //intialize wordlist to contain the three randomly chosen words
             var words = new List<string>();
+            //Variable for index to use in for loop
             var w = new Random();
 
-            for (int i = 0; i<3; i++)
+            //For loop to generate three random words
+            for (int i = 0; i < 3; i++)
             {
                 myRandomIndex = w.Next(wordList.Count);
                 words.Add(wordList[myRandomIndex]);
-               
             }
 
             Console.WriteLine(string.Join(" ", words));
-            
-        }
+
+            do
+            {
+                cki = Console.ReadKey();
+                Console.Write(" --- You pressed ");
+            }
 
 
     }
