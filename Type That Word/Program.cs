0dc628c9312192ac70b45005077f269f72a13d2c
Richard Maness <rickrmaness@gmail.com>
2021-03-09 18:20:45 -0500

    changed list length for difficulty settings, trimmed leading spaces from sentences, and fixed issue with randomness for sentence generation

--- Type That Word/Program.cs
+++ Type That Word/Program.cs
@@ -21,17 +21,17 @@ namespace Type_That_Word
             switch (difficulty)
             {
                 case 1:
-                    listLength = 3;
+                    listLength = 1;
                     break;
                 case 2:
-                    listLength = 5;
+                    listLength = 3;
                     break;
                 case 3:
-                    listLength = 7;
+                    listLength = 5;
                     break;
             }
 
-           //Initializing var for the file with words
+            //Initializing var for the file with words
             var wordFile = File.ReadLines(@"../../../Sentences.txt");
             var WordList = new List<Word>();
 
@@ -44,43 +44,45 @@ namespace Type_That_Word
                     string[] part = lines.Split(".");
                     var sentenceToType = new Word();
                     Int32.TryParse(part[0], out sentenceToType.lineNumber);
-                    sentenceToType.sentence = part[1];
+                    char[] charsToTrim = { ' ' };
+                    
+                    sentenceToType.sentence = part[1].Trim(charsToTrim);
                     WordList.Add(sentenceToType);
 
                 }
                 catch (Exception e)
                 {
-                    
+
                 }
             }
 
             //Variable for index to use in for loop
             var w = new Random();
             var typingTest = new List<Word>();
-            
+
             //For loop to generate three random words
-          for (int i = 0; i < listLength; i++)
+            for (int i = 0; i < listLength; i++)
             {
-               var myRandomIndex = w.Next(typingTest.Count);
-               typingTest.Add(WordList[myRandomIndex]);
+                var myRandomIndex = w.Next(WordList.Count);
+                typingTest.Add(WordList[myRandomIndex]);
             }
 
             //Converting random word list to string and displaying it
-           string stringToType = string.Join(" ", typingTest);
+            string stringToType = string.Join(" ", typingTest);
             Console.WriteLine(stringToType);
 
             // for loop to move the comparison check through all characters of randomly generated words
-           for (int i = 0; i < stringToType.Length;)
+            for (int i = 0; i < stringToType.Length;)
             {
                 //user input converted to string to check against generated words
                 ConsoleKeyInfo userInputGame;
                 userInputGame = Console.ReadKey(true);
 
                 // if statement to compare characters from generated words and user input
-               if (userInputGame.KeyChar == stringToType[i])
+                if (userInputGame.KeyChar == stringToType[i])
                 {
                     Console.Write(userInputGame.KeyChar);
-                   i++;
+                    i++;
                 }
                 else
                 {
