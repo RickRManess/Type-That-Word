558376d32f820e9ffddac55cd80d7fac57270e48
Richard Maness <rickrmaness@gmail.com>
2021-03-08 19:13:34 -0500

    changed typing game to pull from text file with sentences instead of list of words

--- Type That Word/Program.cs
+++ Type That Word/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Type_That_Word
@@ -9,9 +10,9 @@ namespace Type_That_Word
         static void Main(string[] args)
         {
             //Introduction Text
-            Console.WriteLine("Type the words as quick as you can");
+            Console.WriteLine("Type the words as quick as you can!!");
             Console.WriteLine("Try to avoid the BEEEEP");
-            Console.WriteLine("Press 1 for easy 2 for medium 3 for hard");
+            Console.WriteLine("Press: 1= easy game 2= medium game 3= hard game");
 
             //switch case for difficulty
             int difficulty = Int32.Parse(Console.ReadLine());
@@ -30,39 +31,56 @@ namespace Type_That_Word
                     break;
             }
 
-            //Variable for Random index
-            int myRandomIndex = 0;
-            //Initialize the wordlist with the words to choose from
-            var wordList = new List<string> { "apple", "bird", "cat", "dog", "elephant", "fish", "goat" };
+           //Initializing var for the file with words
+            var wordFile = File.ReadLines(@"../../../Sentences.txt");
+            var WordList = new List<Word>();
 
-            //intialize wordlist to contain the three randomly chosen words
-            var words = new List<string>();
+            //foreach loop to split text file into sentences
+
+            foreach (var lines in wordFile)
+            {
+                try
+                {
+                    string[] part = lines.Split(".");
+                    var sentenceToType = new Word();
+                    Int32.TryParse(part[0], out sentenceToType.lineNumber);
+                    sentenceToType.sentence = part[1];
+                    WordList.Add(sentenceToType);
+
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+
             //Variable for index to use in for loop
             var w = new Random();
-
+            var typingTest = new List<Word>();
+            
             //For loop to generate three random words
-            for (int i = 0; i < listLength; i++)
+          for (int i = 0; i < listLength; i++)
             {
-                myRandomIndex = w.Next(wordList.Count);
-                words.Add(wordList[myRandomIndex]);
+               var myRandomIndex = w.Next(typingTest.Count);
+               typingTest.Add(WordList[myRandomIndex]);
             }
 
             //Converting random word list to string and displaying it
-            string stringToType = string.Join(" ", words);
+           string stringToType = string.Join(" ", typingTest);
             Console.WriteLine(stringToType);
 
             // for loop to move the comparison check through all characters of randomly generated words
-            for (int i = 0; i < stringToType.Length;)
+           for (int i = 0; i < stringToType.Length;)
             {
                 //user input converted to string to check against generated words
                 ConsoleKeyInfo userInputGame;
                 userInputGame = Console.ReadKey(true);
 
                 // if statement to compare characters from generated words and user input
-                if (userInputGame.KeyChar == stringToType[i])
+               if (userInputGame.KeyChar == stringToType[i])
                 {
                     Console.Write(userInputGame.KeyChar);
-                    i++;
+                   i++;
                 }
                 else
                 {
--- Type That Word/Word.cs
+++ Type That Word/Word.cs
@@ -6,7 +6,13 @@ namespace Type_That_Word
 {
     class Word
     {
-       
 
+        public int lineNumber;
+        public string sentence;
+
+        public override string ToString()
+        {
+            return $"{sentence}";
+        }
     }
 }
